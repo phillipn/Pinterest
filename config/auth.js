498ccf938bc5eb8a05f59136c4eb6673db605b4f
Nick Phillips <phillipn101@gmail.com>
2016-10-11 11:35:42 -0500

    Fix heroku bug

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'IOjUsQaQjt9HIKbmFrHl7VVE5',
+		'consumerSecret' 	: 'wGjYSbAtQijS2kg53mauMVMQJcBfAqQrgHLnwEVE3dwSEitVap',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	}
+
+};
